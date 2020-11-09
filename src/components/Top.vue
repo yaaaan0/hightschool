433b0d28b848da4c826065bf319ac05041e217c3
yaaaan0 <chun8415@gmail.com>
2020-11-09 14:00:17 +0800

    上傳

---
+++ src/components/Top.vue
@@ -0,0 +1,12 @@
+<template lang="pug">
+  #top
+    //- Vue 的 img src 用 v-bind 時 ./ 代表 public 資料夾
+    //- 沒有用 v-bind 的話會找 src
+    img(:src="'./images/logo.png'")
+</template>
+
+<script>
+export default {
+  name: 'Top'
+}
+</script>
